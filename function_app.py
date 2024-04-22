9128b71e491b49da8cd360d71f8df6b875340ff0
Hellen Ruthes <hellenruthes@gmail.com>
2024-04-22 12:32:16 -0300

    test3

--- function_app.py
+++ function_app.py
@@ -1,9 +1,9 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION, route_prefix="api")
+#app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION, route_prefix="api")
 
-@app.route(route="hellen_test1")
+#@app.route(route="hellen_test1")
 def hellen_test1(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
